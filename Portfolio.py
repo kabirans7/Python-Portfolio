145eda1eb0b2332776ee6657b602eba58d6b985d
Kabir Ansari <kabir.ansari@outlook.com>
2025-03-29 15:51:02 -0400

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -208,52 +208,9 @@ for row in locations_with_no_departments:
 conn.close()
 
 
-#Problem Five Task 1
+#Problem Five Task 1 is in the DBOperations.py file
 
-def create_Sqlite_connection(location_path, database_name):
-    import sqlite3
-    global connection
-    connection = None
-    location=location_path
-    dbase= database_name
-    conn=location+dbase
-
-    #check the location of the database
-    print(conn)
-
-    #create new database
-    connection= sqlite3.connect(conn)
-    print('Database created.')
-    connection.commit()
-    print('Changes saved.')
-    return connection
-
-
-def readcsv(loca, filename):
-    import pandas as pd
-    loc=loca
-    fname=filename
-    locate_file=loc+fname
-    print(locate_file)
-    df=pd.read_csv(locate_file)
-    return df
-
-def writecsv_to_db(dataframe, table_name):
-    import pandas as pd
-    df1=dataframe
-    table=table_name
-    print(table)
-    df1.to_sql(table, connection, if_exists='replace', index=False)
-    return table_name
-
-def displaydbtable (query_string, dataframe):
-    import pandas as pd
-    query_str=query_string
-    print(query_str)
-    dataframe=pd.read_sql(query_str, connection)
-    return dataframe
-
-#Problem Five Task 2
+#Problem Five Task 2 (imports DBOperations.py)
 
 import DBOperations as dbo
 import pandas as pd
